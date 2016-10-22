4cde297f7a937e9003b68833f067a18d7d577431
Vgr E. Barry <vgr255@live.ca>
2016-10-22 01:16:23 -0400

    Add new functions to get or add channels, or check for an existing one
    This new simple API helps to check for existing channels, or create new
    ones on the fly.

--- src/channel.py
+++ src/channel.py
@@ -2,7 +2,24 @@ from src.context import IRCContext
 
 Main = None # main channel
 
+all_channels = {}
+
 _states = ("not yet joined", "pending join", "joined", "pending leave", "left channel", "", "quit", "deleted", "cleared")
+
+get = all_channels.__getitem__
+
+def add(name, cli):
+    """Add and return a new channel, or an existing one if it exists."""
+
+    if name in all_channels:
+        if cli is not all_channels[name].client:
+            raise RuntimeError("different IRC client for channel {0}".format(name))
+        return all_channels[name]
+
+    chan = all_channels[name] = Channel(name, cli)
+    return chan
+
+exists = all_channels.__contains__
 
 class Channel(IRCContext):
 
