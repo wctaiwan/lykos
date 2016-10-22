5f1c1080d61de4fe2980d9ac2a1b1a7b5e748aa6
Vgr E. Barry <vgr255@live.ca>
2016-10-22 11:17:47 -0400

    Add support for WHO requests in hooks
    This adds a function hooks.who, which is used to send out WHO requests.
    The function properly handles servers with or without WHOX support.
    
    This also adds hooks for the two possible kinds of WHO replies; one for
    servers that support WHOX and one for those that don't. This is,
    however, completely transparent to the user code, since both hooks fire
    the exact same event, and feed it the relevant information.
    
    This also adds a hook for the end of a WHO request, which also fires an
    event. For most cases, this is the only event the code will need to hook
    into, since the users will be available in the user registry by the time
    the WHO responses have been fully processed.

--- src/hooks.py
+++ src/hooks.py
@@ -8,6 +8,183 @@ further in the relevant hook functions.
 
 from src.decorators import hook
 from src.context import Features
+from src.events import Event
+
+from src import channel, user, settings as var
+
+### WHO/WHOX requests and responses handling
+
+# Always use this function whenever sending out a WHO request!
+
+def who(cli, target, data=b""):
+    """Send a WHO request with respect to the server's capabilities.
+
+    To get the WHO replies, add an event listener for "who_result", and
+    an event listener for "who_end" for the end of WHO replies.
+
+    The return value of this function is an integer equal to the data
+    given. If the server supports WHOX, the same integer will be in the
+    event.params.data attribute. Otherwise, this attribute will be 0.
+
+    """
+
+    if isinstance(data, str):
+        data = data.encode(Features["CHARSET"])
+    elif isinstance(data, int):
+        if data > 0xFFFFFF:
+            data = b""
+        else:
+            data = data.to_bytes(3, "little")
+
+    if len(data) > 3:
+        data = b""
+
+    if Features["WHOX"]:
+        cli.send("WHO", target, b"%tcuihsnfdlar," + data)
+    else:
+        cli.send("WHO", target)
+
+    return int.from_bytes(data, "little")
+
+@hook("whoreply")
+def who_reply(cli, bot_server, bot_nick, chan, ident, host, server, nick, status, hopcount_gecos):
+    """Handle WHO replies for servers without WHOX support.
+
+    Ordering and meaning of arguments for a bare WHO response:
+
+    0 - The IRCClient instance (like everywhere else)
+    1 - The server the requester (i.e. the bot) is on
+    2 - The nickname of the requester (i.e. the bot)
+    3 - The channel the request was made on
+    4 - The ident of the user in this reply
+    5 - The hostname of the user in this reply
+    6 - The server the user in this reply is on
+    7 - The nickname of the user in this reply
+    8 - The status (H = Not away, G = Away, * = IRC operator, @ = Opped in the channel in 4, + = Voiced in the channel in 4)
+    9 - The hop count and realname (gecos)
+
+    This fires off the "who_result" event, and dispatches it with three
+    arguments, the game state namespace, a Channel, and a User. Less
+    important attributes can be accessed via the event.params namespace.
+
+    """
+
+    hop, realname = hopcount_gecos.split(None, 1)
+    hop = int(hop)
+
+    ch = None
+    modes = {Features["PREFIX"].get(s) for s in status} - {None}
+
+    if nick == bot_nick:
+        user.Bot.nick = nick
+        cli.ident = user.Bot.ident = ident
+        cli.hostmask = user.Bot.host = host
+        cli.real_name = user.Bot.realname = realname
+
+    try:
+        val = user.get(nick, ident, host, realname, allow_bot=True)
+    except KeyError:
+        val = user.add(cli, nick=nick, ident=ident, host=host, realname=realname)
+
+    ch = channel.add(chan, cli)
+    val.channels[ch] = modes
+    ch.users.add(val)
+    for mode in modes:
+        if mode not in ch.modes:
+            ch.modes[mode] = set()
+        ch.modes[mode].add(val)
+
+    event = Event("who_result", {}, status=status, data=0, ip_address=None, server=server, hop_count=hop, idle_time=None, extended_who=False)
+    event.dispatch(var, ch, val)
+
+@hook("whospcrpl")
+def extended_who_reply(cli, bot_server, bot_nick, data, chan, ident, ip_address, host, server, nick, status, hop, idle, account, realname):
+    """Handle WHOX responses for servers that support it.
+
+    An extended WHO (WHOX) is caracterised by a second parameter to the request
+    That parameter must be '%' followed by at least one of 'tcuihsnfdlar'
+    If the 't' specifier is present, the specifiers must be followed by a comma and at most 3 bytes
+    This is the ordering if all parameters are present, but not all of them are required
+    If a parameter depends on a specifier, it will be stated at the front
+    If a specifier is not given, the parameter will be omitted in the reply
+
+    Ordering and meaning of arguments for an extended WHO (WHOX) response:
+
+    0  -   - The IRCClient instance (like everywhere else)
+    1  -   - The server the requester (i.e. the bot) is on
+    2  -   - The nickname of the requester (i.e. the bot)
+    3  - t - The data sent alongside the request
+    4  - c - The channel the request was made on
+    5  - u - The ident of the user in this reply
+    6  - i - The IP address of the user in this reply
+    7  - h - The hostname of the user in this reply
+    8  - s - The server the user in this reply is on
+    9  - n - The nickname of the user in this reply
+    10 - f - Status (H = Not away, G = Away, * = IRC operator, @ = Opped in the channel in 5, + = Voiced in the channel in 5)
+    11 - d - The hop count
+    12 - l - The idle time (or 0 for users on other servers)
+    13 - a - The services account name (or 0 if none/not logged in)
+    14 - r - The realname (gecos)
+
+    This fires off the "who_result" event, and dispatches it with three
+    arguments, the game state namespace, a Channel, and a User. Less
+    important attributes can be accessed via the event.params namespace.
+
+    """
+
+    if account == "0":
+        account = None
+
+    hop = int(hop)
+    idle = int(idle)
+
+    data = int.from_bytes(3, data.encode(Features["CHARSET"]), "little")
+
+    ch = None
+    modes = {Features["PREFIX"].get(s) for s in status} - {None}
+
+    if nick == bot_nick:
+        user.Bot.nick = nick
+        cli.ident = user.Bot.ident = ident
+        cli.hostmask = user.Bot.host = host
+        cli.real_name = user.Bot.realname = realname
+        user.Bot.account = account
+
+    try:
+        val = user.get(nick, ident, host, realname, account, allow_bot=True)
+    except KeyError:
+        val = user.add(cli, nick=nick, ident=ident, host=host, realname=realname, account=account)
+
+    ch = channel.add(chan, cli)
+    val.channels[ch] = modes
+    ch.users.add(val)
+    for mode in modes:
+        if mode not in ch.modes:
+            ch.modes[mode] = set()
+        ch.modes[mode].add(val)
+
+    event = Event("who_result", {}, status=status, data=data, ip_address=ip_address, server=server, hop_count=hop, idle_time=idle, extended_who=True)
+    event.dispatch(var, ch, val)
+
+@hook("endofwho")
+def end_who(cli, bot_server, bot_nick, target, rest):
+    """Handle the end of WHO/WHOX responses from the server.
+
+    Ordering and meaning of arguments for the end of a WHO/WHOX request:
+
+    0 - The IRCClient instance (like everywhere else)
+    1 - The server the requester (i.e. the bot) is on
+    2 - The nickname of the requester (i.e. the bot)
+    3 - The target the request was made against
+    4 - A string containing some information; traditionally "End of /WHO list."
+
+    This fires off the "who_end" event, and dispatches it with two
+    arguments: The game state namespace and a str of the request that
+    was originally sent.
+
+    """
+
+    Event("who_end", {}).dispatch(var, target)
 
 ### Server PING handling
 
