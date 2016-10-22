d12171b42ea097d008b6e909d8b47ff9272f540f
Vgr E. Barry <vgr255@live.ca>
2016-10-22 11:03:24 -0400

    Add some docstrings to hooks

--- src/hooks.py
+++ src/hooks.py
@@ -1,3 +1,11 @@
+"""Handlers and dispatchers for IRC hooks live in this module.
+
+Most of these hooks fire off specific events, which can be listened to
+by code that wants to operate on these events. The events are explained
+further in the relevant hook functions.
+
+"""
+
 from src.decorators import hook
 from src.context import Features
 
@@ -5,13 +13,31 @@ from src.context import Features
 
 @hook("ping")
 def on_ping(cli, prefix, server):
+    """Send out PONG replies to the server's PING requests.
+
+    Ordering and meaning of arguments for a PING request:
+
+    0 - The IRCClient instance (like everywhere else)
+    1 - Nothing (always None)
+    2 - The server which sent out the request
+
+    """
+
     cli.send("PONG", server)
 
 ### Fetch and store server information
 
 @hook("featurelist")
 def get_features(cli, rawnick, *features):
-    """Fetch and store the IRC server features."""
+    """Fetch and store the IRC server features.
+
+    Ordering and meaning of arguments for a feature listing:
+
+    0 - The IRCClient instance(like everywhere else)
+    1 - The raw nick (nick!ident@host) of the requester (i.e. the bot)
+    * - A variable number of arguments, one per available feature
+
+    """
 
     for feature in features:
         if "=" in feature:
